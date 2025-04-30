7623b88850403ec6e140d3c9cb61c5c728e0c089
Blessings <134072395+sparechange679@users.noreply.github.com>
2025-04-30 20:07:02 +0200

    refactor autoloading and reorganize core files  - Implemented a spl_autoload_register function for dynamic class loading - Renamed and relocated Database.php, Response.php, and Validator.php to improve structure - Updated index.php to utilize the new autoloading mechanism

--- public/index.php
+++ public/index.php
@@ -4,6 +4,8 @@ $BASE_PATH = __DIR__ . '/../';
 
 require $BASE_PATH . 'functions.php';
 
-require base_path('Database.php');
-require base_path('Response.php');
+spl_autoload_register(function ($class) {
+    require base_path("Core/{$class}.php");
+});
+
 require base_path('router.php');
---
+++ routes.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    '/' => 'controllers/auth/index.php',
+    '/admin-login' => 'controllers/auth/admin_login.php',
+    '/admin-register' => 'controllers/auth/admin_register.php',
+    '/client-login' => 'controllers/auth/client_login.php',
+    '/client-register' => 'controllers/auth/client_register.php',
+    '/agent-login' => 'controllers/auth/agent_login.php',
+    '/keeper-login' => 'controllers/auth/keeper_login.php',
+];
